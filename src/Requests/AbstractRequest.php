433ab7c249ed6a80d035eb90ff2138b4bfff7aef
Ismail Özgün Turan <oezguen.turan@dadadev.com>
2019-08-21 08:55:25 +0200

    add locale constants

--- src/Requests/AbstractRequest.php
+++ src/Requests/AbstractRequest.php
@@ -22,6 +22,22 @@ use JMS\Serializer\Annotation as JMS;
  */
 abstract class AbstractRequest
 {
+    private const LOCALE_GERMAN_GERMAN = 'de-DE';
+    private const LOCALE_AUSTRALIAN_ENGLISH = 'en-AU';
+    private const LOCALE_CANADIAN_ENGLISH = 'en-CA';
+    private const LOCALE_BRITISH_ENGLISH = 'en-GB';
+    private const LOCALE_INDIAN_ENGLISH = 'en-IN';
+    private const LOCALE_AMERICAN_ENGLISH = 'en-US';
+    private const LOCALE_SPANISH_SPANISH = 'es-ES';
+    private const LOCALE_MEXICAN_SPANISH = 'es-MX';
+    private const LOCALE_AMERICAN_SPANISH = 'es-US';
+    private const LOCALE_CANADIAN_FRENCH = 'fr-CA';
+    private const LOCALE_FRENCH_FRENCH = 'fr-FR';
+    private const LOCALE_INDIAN_HINDI = 'hi-IN';
+    private const LOCALE_ITALIAN_ITALIAN = 'it-IT';
+    private const LOCALE_JAPANESE_JAPANESE = 'ja-JP';
+    private const LOCALE_BRAZILIAN_PORTUGUESE = 'pt-BR';
+
     public const TYPE_LAUNCH_REQUEST = 'LaunchRequest';
     public const TYPE_INTENT_REQUEST = 'IntentRequest';
     public const TYPE_SESSION_ENDED_REQUEST = 'SessionEndedRequest';
