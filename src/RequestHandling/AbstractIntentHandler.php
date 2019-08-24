d40457c9e9330474c74658cc3fb39b2a76babb72
Ismail Özgün Turan <oezguen.turan@dadadev.com>
2019-08-24 11:42:34 +0200

    extend abstract intent handler

--- src/RequestHandling/AbstractIntentHandler.php
+++ src/RequestHandling/AbstractIntentHandler.php
@@ -8,28 +8,48 @@ use DaDaDev\AmazonAlexa\Requests\AbstractRequest;
 abstract class AbstractIntentHandler implements IntentHandlerInterface
 {
     /**
-     * @return string
-     */
-    private function getRequestType(): string
-    {
-        return AbstractRequest::TYPE_INTENT_REQUEST;
-    }
-
-    /**
      * @param Request $alexaRequest
      *
      * @return bool
      */
     public function canFulfilRequestedIntent(Request $alexaRequest): bool
     {
-        if ($this->getRequestType() !== $alexaRequest->getRequestType()) {
+        if (!$this->isAllowedRequestType($alexaRequest->getRequestType())) {
             return false;
         }
 
-        if ($this->getIntentName() !== $alexaRequest->getIntent()) {
+        if (!$this->isAllowedIntentName($alexaRequest->getIntent())) {
             return false;
         }
 
         return true;
+    }
+
+    /**
+     * @param string $requestType
+     *
+     * @return bool
+     */
+    protected function isAllowedRequestType(string $requestType): bool
+    {
+        return $this->getRequestType() === $requestType;
+    }
+
+    /**
+     * @param string $intentName
+     *
+     * @return bool
+     */
+    protected function isAllowedIntentName(string $intentName): bool
+    {
+        return $this->getIntentName() === $intentName;
+    }
+
+    /**
+     * @return string
+     */
+    private function getRequestType(): string
+    {
+        return AbstractRequest::TYPE_INTENT_REQUEST;
     }
 }
