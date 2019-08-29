9e55226ff630f1c529d2fc8d47b0cc18d416e0f4
Ismail Özgün Turan <oezguen.turan@dadadev.com>
2019-08-29 11:56:12 +0200

    fix wrong definition of value in values object

--- src/Requests/RequestTypes/IntentElements/Values.php
+++ src/Requests/RequestTypes/IntentElements/Values.php
@@ -7,15 +7,15 @@ use JMS\Serializer\Annotation as JMS;
 class Values
 {
     /**
-     * @var Value[]|null
-     * @JMS\Type("array<DaDaDev\AmazonAlexa\Requests\RequestTypes\IntentElements\Value>")
+     * @var Value|null
+     * @JMS\Type("DaDaDev\AmazonAlexa\Requests\RequestTypes\IntentElements\Value")
      */
     protected $value;
 
     /**
-     * @return Value[]|null
+     * @return Value|null
      */
-    public function getValue(): ?array
+    public function getValue(): ?Value
     {
         return $this->value;
     }
